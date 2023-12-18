d0b9f5c15f45b983d49d8a750051874b1e672c42
hello-kitsch <jylucia2002@naver.com>
2023-12-18 17:18:22 +0900

    1218

--- studyLog/thisIsJava/src/ch14/sec06/exam01/SynchronizedExample.java
+++ studyLog/thisIsJava/src/ch14/sec06/exam01/SynchronizedExample.java
@@ -6,10 +6,11 @@ public class SynchronizedExample {
 
         User1Thread user1Thread = new User1Thread();
         user1Thread.setCalculator(calculator);
-        user1Thread.start();
 
         User2Thread user2Thread = new User2Thread();
         user2Thread.setCalculator(calculator);
+        
+        user1Thread.start();
         user2Thread.start();
     }
 }
